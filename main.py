45d2d2ccb5ee0ce7f13f23d81aa62ce6e8c37ac7
Balaji B <balaji.balachandar1@gmail.com>
2024-08-27 20:52:06 +0530

    Added Search feature

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from random import randint,choice,shuffle
 import pyperclip
 import json
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generated_password():
     pass_input.delete(0, END)
@@ -53,8 +54,35 @@ def save():
             website_input.focus()
             messagebox.showinfo(title="",message="Saved Successfully")
 
-# ---------------------------- UI SETUP ------------------------------- #
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def find_password():
+    website = website_input.get()
+    try:
+        with open("password_db.json", mode="r") as file:
+            data = json.load(file)
+        if website in data:
+            messagebox.showinfo(title=website, message=f"Email : {data[website]["email"]}\n"
+                                                       f"Password : "f"{data[website]["password"]}")
+        elif len(website) == 0:
+             messagebox.showinfo(title="Are you blind?", message="Text Field Is Empty")
+        else:
+            messagebox.showinfo(title="Error", message=f"No Details Of The {website} Exists")
 
+        # for i in data:
+        #     if website == i:
+        #         messagebox.showinfo(title=website,message=f"Email : {data[i]["email"]}\nPassword : "
+        #                                                     f"{data[i]["password"]}")
+        #     elif len(website) == 0:
+        #         messagebox.showinfo(title="Are you blind?", message="Text Field Is Empty")
+        #         break
+        #     elif website not in data:
+        #         messagebox.showinfo(title="Error", message="No Details Of The Website Exists")
+        #         break
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+
+# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20,pady=20)
@@ -64,35 +92,38 @@ logo_img = PhotoImage(file = "logo.png")
 canvas.create_image(100,100,image = logo_img)
 canvas.grid(row = 0,column = 1)
 
-# labels--------------------------------------------------------------------------------------------------------------
-website_text = Label(text="Website:")
+# LABELS--------------------------------------------------------------------------------------------------------------
+website_text = Label(text="Website:",padx=5,pady=5)
 website_text.grid(row = 1,column = 0)
 
-email_text = Label(text="Email/Username:")
+email_text = Label(text="Email/Username:",padx=5,pady=5)
 email_text.grid(row = 2,column = 0)
 
-pass_text = Label(text="Password:")
+pass_text = Label(text="Password:",padx=5,pady=5)
 pass_text.grid(row = 3,column = 0)
 
-# entries--------------------------------------------------------------------------------------------------------------
-website_input = Entry(width=35)
-website_input.grid(row = 1,column = 1, columnspan=2, sticky = EW)
+# ENTRIES--------------------------------------------------------------------------------------------------------------
+website_input = Entry(width=32)
+website_input.grid(row = 1,column = 1, sticky = W)
 website_input.focus()
-# the "sticky" parameter, the EW part is the compass directions (E)ast and (W)est and the sticky basically "sticks" the
-# widget to the edges of the column.
 
 email_input = Entry(width=35)
 email_input.grid(row = 2,column = 1, columnspan=2, sticky = EW)
 
-pass_input = Entry(width=21)
-pass_input.grid(row = 3,column = 1, sticky = EW)
+pass_input = Entry(width=32)
+pass_input.grid(row = 3,column = 1, sticky = W)
 
-# buttons------------------------------------------------------------------------------------------------------------
+# BUTTONS------------------------------------------------------------------------------------------------------------
+search_pass = Button(text = "Search", command=find_password)
+search_pass.grid(row = 1,column = 2, sticky = EW)
+# the "sticky" parameter, the EW part is the compass directions (E)ast and (W)est and the sticky basically "sticks" the
+# widget to the edges of the column.
+
 generate_pass = Button(text = "Generate Password", command=generated_password)
 generate_pass.grid(row = 3,column = 2, sticky = EW)
 
 add_pass = Button(text = "Add",width=36, command=save)
-add_pass.grid(row = 4,column = 1, columnspan=2, sticky = EW)
+add_pass.grid(row = 4,column = 1, columnspan=2, sticky = EW,pady=5)
 
 
 window.mainloop()
